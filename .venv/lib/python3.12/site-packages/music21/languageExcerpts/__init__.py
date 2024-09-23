03c4fde5751c699d10a0e95733d85bde84e801ca
Lisa <62295597+lalisalala@users.noreply.github.com>
2024-09-23 12:57:12 +0200

    Delete .venv directory

--- .venv/lib/python3.12/site-packages/music21/languageExcerpts/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-
-from music21.languageExcerpts import instrumentLookup
-from music21.languageExcerpts import naturalLanguageObjects
--- .venv/lib/python3.12/site-packages/music21/lily/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-
-__all__ = ['translate', 'lilyObjects']
-
-from music21.lily import translate
-from music21.lily import lilyObjects
